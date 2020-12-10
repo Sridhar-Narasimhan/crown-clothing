345c8137a731343f8f01014edfd62c9cd63acd00
sridharn <sridharn@syncfusion.com>
2020-12-10 09:07:16 +0530

    Created homepage for application

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Homepage from './components/homepage.component';
+function App() {
+  return (
+    <div className="App">
+     <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
